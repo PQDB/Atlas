b3660d3f559f37117163227d6dd454220c574c60
Guillaume Larocque <glaroc@gmail.com>
2014-09-15 10:07:37 -0400

    Added first scripts

---
+++ GDAL_OGR/defineProjection.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in $(ls *.shp); 
+
+do
+
+echo “Begin Processing!”
+
+ogr2ogr -f “ESRI Shapefile” -a_srs “EPSG:32198″ ./projectedShapefiles $i
+
+echo “Processing Done!”
+
+done
---
+++ GDAL_OGR/mergeShapefiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+
+
+DATA=`find . -name '*.shp'`
+
+ogr2ogr -a_srs EPSG:32198 mergedShapeFile.shp
+
+
+for i in $DATA
+
+do
+ogr2ogr -append -update mergedShapeFile.shp $i -f "Esri Shapefile"
+
+
+done
