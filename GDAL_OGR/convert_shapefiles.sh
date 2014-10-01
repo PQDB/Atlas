d29889a1b444024d1a768abbc761e22b599fbf8c
Telahnus <quan.jordan@gmail.com>
2014-10-01 10:30:10 -0400

    Added ability to convert a single designated shapefile

--- GDAL_OGR/convert_shapefiles.sh
+++ GDAL_OGR/convert_shapefiles.sh
@@ -2,13 +2,19 @@
 #Jordan Quan Sept29, 2014
 
 #Place script in desired folder
-#Convert all shapefiles in current directory to EPSG:32198 projection
+#Reprojects shapefiles to EPSG:32198
+#Input name of file to convert one, or leave blank to convert all in folder
 #The converted shapefiles are place in the converted subdirectory
 
 mkdir -p ./converted
 
-for shp in *.shp
-do
- echo "Processing $shp"
- ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:32198 converted/$shp $shp
-done
+if [ "$#" = "1" ]; then
+	echo "Reprojecting $1"
+	ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:32198 converted/$1 $1
+else
+	for shp in *.shp
+	do
+ 		echo "Reprojecting $shp"
+ 		ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:32198 converted/$shp $shp
+	done
+fi
