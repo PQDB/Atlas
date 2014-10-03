18fa17784a504cf72d311cc2a25e404832bda047
Telahnus <quan.jordan@gmail.com>
2014-10-03 15:56:30 -0400

    Clip script finally works! Completely! Huzzah!

--- GDAL_OGR/clip_shapefiles.sh
+++ GDAL_OGR/clip_shapefiles.sh
@@ -1,87 +1,59 @@
 #!/bin/bash
 # Jordan Quan, Oct1, 2014
-
-#Place script in desired folder
 #Clips shapefiles using a bounding.shp
-#Input name of file to clip one, or leave blank to clip all in folder
-#The clipped shapefiles are placed in the clipped subdirectory
 
-#args $1: -b=clip to box only, -p=clip to polygon
+#Place script in folder with desired shapefiles
+#MAKE SURE ALL FILES ARE IN SAME REFERENCE SYSTEM!!
+#IE. RUN CONVERT_SHAPEFILES.SH FIRST
+#input arguments: script, cliptype, mask, input
+#The clipped shapefiles are placed in boxclip and polyclip subdirectories
+#NOTE: cant overwrite, will cause error
+
+#ARGUMENTS
+#args $1: -b clips to box, -p clips to poly (takes much longer)
 #args $2: mask.shp
-#args $3: input.shp (optional)
+#args $3: input.shp (optional, leave blank to clip all in folder
 
+#making output folders
 mkdir -p ./boxclip
 mkdir -p ./polyclip
 
+#calculate clipping mask
 MASK=$2
-INPUT=$3
-OUTPUT="output.shp"
-#TEMP="temp.shp"
 BASE=`basename $MASK .shp`
-
+echo "Determining extent of $MASK..."
 EXTENT=`ogrinfo -so $MASK $BASE | grep Extent \
-| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
-| sed 's/ - /, /g'`
-
+			| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
+			| sed 's/ - /, /g'`
 EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
+echo "Extents are: $EXTENT"
 
-echo $EXTENT
-
-ogr2ogr -clipsrc $EXTENT boxclip/$INPUT $INPUT
-
-echo "box clip done"
-
-
-
-if false; then
-	if [ "$#" = "2" ];then	#clip all files
-		for shp in *.shp
-		do
-			if [ "$1" = "-b" ]; then #using box extents
-				echo "clipping $shp to box"
-				EXTENT=`ogrinfo -so $MASK $BASE | grep Extent \
-					| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
-					| sed 's/ - /, /g'`
-				EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
-				ogr2ogr -f "ESRI Shapefile" /boxclip/$shp $shp -clipsrc $EXTENT
-				echo "boxclip complete"
-			elif [ "$1" = "-p" ]; then
-				echo "clipping $shp to box"
-				EXTENT=`ogrinfo -so $MASK $BASE | grep Extent \
-					| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
-					| sed 's/ - /, /g'`
-				EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
-				ogr2ogr -f "ESRI Shapefile" /boxclip/$shp $shp -clipsrc $EXTENT
-				echo "boxclip complete"
-				echo "clipping $shp to poly"
-				ogr2ogr -clipsrc $MASK /polyclip/$shp /boxclip/$shp
-				echo "polyclip complete"
-			fi
-		done
-		echo "All clipping complete"
-	elif [ "$#" = "3" ]; then
-		$INPUT = $3
-		if [ "$1" = "-b" ]; then #using box extents
-			echo "clipping $3 to box"
-			EXTENT=`ogrinfo -so $MASK $BASE | grep Extent \
-				| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
-				| sed 's/ - /, /g'`
-			EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
-			ogr2ogr -f "ESRI Shapefile" /boxclip/$INPUT $INPUT -clipsrc $EXTENT
-			echo "boxclip complete"
-		elif [ "$1" = "-p" ]; then
-			echo "clipping $shp to box"
-			EXTENT=`ogrinfo -so $MASK $BASE | grep Extent \
-				| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
-				| sed 's/ - /, /g'`
-			EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
-			ogr2ogr -f "ESRI Shapefile" /boxclip/$INPUT $INPUT -clipsrc $EXTENT
-			echo "boxclip complete"
-			echo "clipping $shp to poly"
-			ogr2ogr -clipsrc $MASK /polyclip/$INPUT /boxclip/$INPUT
+#clip all files
+if [ "$#" = "2" ];then
+	for INPUT in *.shp
+	do
+		echo "clipping $INPUT to box..."
+		ogr2ogr -f "ESRI Shapefile" -clipsrc $EXTENT boxclip/$INPUT $INPUT
+		echo "boxclip complete"
+		if [ "$1" = "-p" ]; then
+			echo "clipping $INPUT to poly..."
+			ogr2ogr -clipsrc $MASK polyclip/$INPUT boxclip/$INPUT
 			echo "polyclip complete"
 		fi
-	else 
-		echo "please input command -b/-p mask input(optional)"
+	done
+	echo "All clipping complete"
+
+#clip specific file
+elif [ "$#" = "3" ]; then
+	INPUT=$3
+	echo "clipping $INPUT to box..."
+	ogr2ogr -f "ESRI Shapefile" -clipsrc $EXTENT boxclip/$INPUT $INPUT
+	echo "boxclip complete"
+	if [ "$1" = "-p" ]; then
+		echo "clipping $INPUT to poly..."
+		ogr2ogr -clipsrc $MASK polyclip/$INPUT boxclip/$INPUT
+		echo "polyclip complete"
 	fi
+else 
+	echo "please input command -b/-p mask input(optional)"
 fi
