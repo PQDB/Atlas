a1d412c2643a316629f275714bc531a2d7e614c6
Telahnus <quan.jordan@gmail.com>
2014-10-03 16:07:09 -0400

    Minor polishing of convert script

--- GDAL_OGR/convert_shapefiles.sh
+++ GDAL_OGR/convert_shapefiles.sh
@@ -9,12 +9,13 @@
 mkdir -p ./converted
 
 if [ "$#" = "1" ]; then
-	echo "Reprojecting $1"
+	echo "Reprojecting $1..."
 	ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:32198 converted/$1 $1
 else
 	for shp in *.shp
 	do
- 		echo "Reprojecting $shp"
+ 		echo "Reprojecting $shp..."
  		ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:32198 converted/$shp $shp
 	done
 fi
+echo "Reprojection complete"
