6da281ec7de3ba08feaebdf78ff103fe7e62bdf9
Jordan Quan <quan.jordan@gmail.com>
2014-11-14 15:35:28 -0500

    script to upload vectors to postgresql database. omg this was a chore!

---
+++ GDAL_OGR/upload_shapefiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# FILE: upload_shapefiles.sh
+# DESC: upload shapefile to PostGIS database
+# AUTHOR: Jordan Quan
+# LAST REVISED: 2014/11/14
+
+#script must be run using 'sudo -u postgres ./upload_shapefiles.sh'
+# uses -W "latin1" cuz of french characters
+
+for shp in *.shp
+do
+	temp="${shp##*/}" 
+	name="${temp%.*}"
+    shp2pgsql -W "latin1" $shp public.$name | psql -d AtlasData
+done
